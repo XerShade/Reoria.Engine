74f0e8150e2732f4ac7772ece21ecedacbc29d93
XerShade <xershade.ca@gmail.com>
2025-03-13 04:52:05 -0400

    feat: add TimeAccumulator

---
+++ Base/Common/TimeAccumulator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Reoria.Engine.Base.Common;
+
+public class TimeAccumulator(double timeStep, double maxUpdates) : Disposable, IDisposable, IAsyncDisposable
+{
+    public readonly double TimeStep = timeStep;
+    public readonly double MaxUpdates = maxUpdates;
+
+    public double Accumulator { get; protected set; } = 0d;
+    public int UpdateCount { get; protected set; } = 0;
+
+    public virtual void Update(GameTime gameTime, Action<GameTime> action)
+    {
+        double deltaTime = gameTime.ElapsedGameTime.TotalSeconds;
+        this.Accumulator += deltaTime;
+        this.UpdateCount = 0;
+
+        while (this.Accumulator >= this.TimeStep && this.UpdateCount < this.MaxUpdates)
+        {
+            action.Invoke(new GameTime(gameTime.TotalGameTime, TimeSpan.FromSeconds(this.TimeStep), gameTime.IsRunningSlowly));
+            this.Accumulator -= this.TimeStep;
+            this.UpdateCount++;
+        }
+    }
+}
