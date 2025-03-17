a563c0ad196f0452794d40fc29aa59a2ff18b7f5
XerShade <xershade.ca@gmail.com>
2025-03-17 05:32:57 -0400

    feat: add ServiceRegistry class
    This allows for runtime injection of service instances for things like MonoGame types that can't be added to the container before initialization of the engine.

---
+++ Base/Container/Services/Interfaces/IServiceRegistry.cs
@@ -0,0 +1,9 @@
+﻿namespace Reoria.Engine.Base.Container.Services.Interfaces;
+
+public interface IServiceRegistry
+{
+    TService FetchService<TService>();
+    void RegisterService<TService>(TService instance);
+    void RemoveService<TService>();
+    void UpdateService<TService>(TService instance);
+}
---
+++ Base/Container/Services/ServiceRegistry.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Logging;
+using Reoria.Engine.Base.Container.Attributes;
+using Reoria.Engine.Base.Container.Services.Interfaces;
+using System.Collections.Concurrent;
+
+namespace Reoria.Engine.Base.Container.Services;
+
+[Container]
+public class ServiceRegistry(ILogger<IServiceRegistry> logger) : IServiceRegistry
+{
+    [ContainerAttribute.DiscoverSerivceDefinitions]
+    public static void DiscoverSerivceDefinitions(ContainerServiceDefinitions services) =>
+        services.AddSingleton<IServiceRegistry, ServiceRegistry>();
+
+    protected readonly ConcurrentDictionary<Type, object> services = [];
+    protected readonly ILogger<IServiceRegistry> logger = logger;
+    protected readonly Lock @lock = new();
+
+    public virtual void RegisterService<TService>(TService instance)
+    {
+        if (instance is null)
+        {
+            throw new ArgumentNullException(nameof(instance));
+        }
+
+        lock (this.@lock)
+        {
+            if (!this.services.ContainsKey(typeof(TService)))
+            {
+                this.services[typeof(TService)] = instance;
+                this.logger.LogDebug("Added new registered instance of service '{service}' to the service registry.", typeof(TService).Name);
+            }
+        }
+    }
+
+    public virtual void UpdateService<TService>(TService instance)
+    {
+        if (instance is null)
+        {
+            throw new ArgumentNullException(nameof(instance));
+        }
+
+        lock (this.@lock)
+        {
+            this.services[typeof(TService)] = instance;
+            this.logger.LogDebug("Updated registered instance of service '{service}' in the service registry.", typeof(TService).Name);
+        }
+    }
+
+    public virtual void RemoveService<TService>()
+    {
+        lock (this.@lock)
+        {
+            if (this.services.ContainsKey(typeof(TService)))
+            {
+                _ = this.services.Remove(typeof(TService), out _);
+                this.logger.LogDebug("Removed registered instance of service '{service}' from the service registry.", typeof(TService).Name);
+            }
+        }
+    }
+
+    public virtual TService FetchService<TService>()
+    {
+        lock (this.@lock)
+        {
+            if (this.services.TryGetValue(typeof(TService), out object? service))
+            {
+                return (TService)service;
+            }
+            throw new InvalidOperationException($"No registered instance for service '{typeof(TService).Name}' not found.");
+        }
+    }
+}
