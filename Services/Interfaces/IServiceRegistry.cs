d446a2bf0a27566ec1dc8aa8644254d5b87ae2b9
XerShade <xershade.ca@gmail.com>
2025-03-31 20:20:10 -0400

    refactor: remove ServiceRegistry class

--- Services/Interfaces/IServiceRegistry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Reoria.Engine.Services.Interfaces;
-
-public interface IServiceRegistry
-{
-    TService FetchService<TService>();
-    void RegisterService<TService>(TService instance);
-    void RemoveService<TService>();
-    void UpdateService<TService>(TService instance);
-}
