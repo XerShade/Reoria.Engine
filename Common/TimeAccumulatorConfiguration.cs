94a9d54ea1d78c7fadecc848620c7ecc93b1a1a0
XerShade <xershade.ca@gmail.com>
2025-04-09 02:45:09 -0400

    feat: add TimeAccumulatorConfiguration

---
+++ Common/TimeAccumulatorConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Reoria.Engine.Common;
+
+public readonly struct TimeAccumulatorConfiguration
+{
+    public readonly double UpdatesPerSecond;
+    public readonly int MaxUpdatesPerFrame;
+
+    public TimeAccumulatorConfiguration()
+    {
+        this.UpdatesPerSecond = 1 / 60;
+        this.MaxUpdatesPerFrame = 5;
+    }
+
+    public TimeAccumulatorConfiguration(IConfigurationSection configuration) : this()
+    {
+        this.UpdatesPerSecond = 1 / Convert.ToDouble(configuration["UpdatesPerSecond"] ?? "60");
+        this.MaxUpdatesPerFrame = Convert.ToInt32(configuration["MaxUpdatesPerFrame"] ?? "5");
+    }
+
+    public TimeAccumulatorConfiguration(double updatesPerSecond, int maxUpdatesPerFrame)
+    {
+        this.UpdatesPerSecond = updatesPerSecond;
+        this.MaxUpdatesPerFrame = maxUpdatesPerFrame;
+    }
+}
